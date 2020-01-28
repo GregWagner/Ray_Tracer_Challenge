22c1268cdb819c8e76ffdb2c187e91d8c33b918c
GregWagner <theGreg101@gmail.com>
2020-01-28 12:42:21 -0600

    Added Tuple class

---
+++ src/Tuple.h
@@ -0,0 +1,17 @@
+#ifndef TUPLE_H
+#define TUPLE_H
+
+class Tuple {
+  public:
+    Tuple(double x, double y, double z, double w) 
+    : x(x), y(y), z(z), w(w) {
+    }
+
+    double x;
+    double y;
+    double z;
+    double w;
+
+};
+
+#endif
