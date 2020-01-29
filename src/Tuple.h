2a887f6e4c15294ccd7f2a29783605895ea6395b
GregWagner <theGreg101@gmail.com>
2020-01-29 08:06:13 -0600

    Made data private

--- src/Tuple.h
+++ src/Tuple.h
@@ -7,6 +7,32 @@ class Tuple {
     : x(x), y(y), z(z), w(w) {
     }
 
+    bool isPoint() const {
+      return w == 1.0;
+    }
+
+    bool isVector() const {
+      return w == 0.0;
+    }
+
+    double getX() const {
+      return x;
+    }
+
+    double getY() const {
+      return y;
+    }
+
+    double getZ() const {
+      return z;
+    }
+
+    double getW() const {
+      return w;
+    }
+
+
+  private:
     double x;
     double y;
     double z;
--- tests/TupleTest.cpp
+++ tests/TupleTest.cpp
@@ -5,10 +5,26 @@ SCENARIO("A tuple with w = 1.0 is a point", "[tuple]") {
   GIVEN("A is a tuple with w = 1.0 ") {
     auto a = Tuple(4.3, -4.2, 3.1, 1.0);
     THEN("a is a point") {
-      REQUIRE(a.x == 4.3);
-      REQUIRE(a.y == -4.2);
-      REQUIRE(a.z == 3.1);
-      REQUIRE(a.w == 1.0);
+      REQUIRE(a.getX() == 4.3);
+      REQUIRE(a.getY() == -4.2);
+      REQUIRE(a.getZ() == 3.1);
+      REQUIRE(a.getW() == 1.0);
+      REQUIRE(a.isPoint());
+      REQUIRE(!a.isVector());
+    }
+  }
+}
+
+SCENARIO("A tuple with w = 0.0 is a vector", "[tuple]") {
+  GIVEN("A is a tuple with w = 0.0 ") {
+    auto a = Tuple(4.3, -4.2, 3.1, 0.0);
+    THEN("a is a vector") {
+      REQUIRE(a.getX() == 4.3);
+      REQUIRE(a.getY() == -4.2);
+      REQUIRE(a.getZ() == 3.1);
+      REQUIRE(a.getW() == 0.0);
+      REQUIRE(!a.isPoint());
+      REQUIRE(a.isVector());
     }
   }
 }
